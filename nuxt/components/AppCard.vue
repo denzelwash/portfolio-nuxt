edbc7e29f97ac99aa7b259d66bacdc124266293a
Denis Obolevich <denzelwash33@mail.ru>
2022-07-22 12:05:05 +0300

    added-cards-component

--- nuxt/components/AppCard.vue
+++ nuxt/components/AppCard.vue
@@ -1,18 +1,49 @@
 <template>
-    <div class="portfolio-item aspect-w-1 aspect-h-1">
-        <img :src="require(`~/assets/img/works/${work.img}`)" />
-        <div class="portfolio-item__wr"></div>
+  <div class="portfolio-item aspect-w-1 aspect-h-1">
+    <img :src="require(`~/assets/img/works/${work.img}`)" />
+    <div
+      class="bg-gray-600 bg-opacity-90 p-5 text-white flex flex-col items-start"
+      :class="{ 'work-rip': work.rip }"
+    >
+      <h4
+        class="text-xl font-medium mb-5 border-amber-400 border-b-2 inline-block"
+      >
+        {{ work.name }}
+      </h4>
+      <ul>
+        <li v-for="item in work.list" :key="item" class="text-sm mb-1">
+          {{ item }}
+        </li>
+      </ul>
+      <div class="mt-auto flex justify-between items-center w-full">
+        <a
+          v-if="!work.rip"
+          :href="work.url"
+          class="underline flex items-center hover:no-underline"
+        >
+          <img
+            src="~assets/img/svg/external.svg"
+            width="12"
+            height="12"
+            class="mr-2"
+          />
+          <span class="text-sm">Перейти</span>
+        </a>
+        <span v-else class="text-sm">Rip</span>
+        <span class="text-sm">{{ work.year || 2019 }}</span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        work: {
-            type: Object,
-            default: null,
-        },
+  props: {
+    work: {
+      type: Object,
+      default: null,
     },
+  },
 }
 </script>
 
--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,75 +1,80 @@
 export default {
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    // ssr: false,
-    head: {
-        title: 'Денис Оболевич ★ Портфолио',
-        htmlAttrs: {
-            lang: 'ru',
-        },
-        meta: [
-            { charset: 'utf-8' },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1',
-            },
-            {
-                hid: 'description',
-                name: 'description',
-                content:
-                    'Frontend разработчик, Денис Оболевич. Портфолио. Разработка сайтов, SPA, дизайн, верстка, программирование.',
-            },
-            {
-                hid: 'keywords',
-                name: 'keywords',
-                content:
-                    'HTML5, CSS3, PHP, JavaScript, Jquery, Vue, Nuxt, Git, LESS/SASS/SCSS, Flexbox, Grid, Adaptive.',
-            },
-            { name: 'format-detection', content: 'telephone=no' },
-        ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  // ssr: false,
+  head: {
+    title: 'Денис Оболевич ★ Портфолио',
+    htmlAttrs: {
+      lang: 'ru',
     },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
-    css: ['@/assets/scss/main'],
-
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
-
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
-
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-    buildModules: [
-        // https://go.nuxtjs.dev/eslint
-        '@nuxtjs/eslint-module',
-        // https://go.nuxtjs.dev/tailwindcss
-        '@nuxtjs/tailwindcss',
+    meta: [
+      { charset: 'utf-8' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Frontend разработчик, Денис Оболевич. Портфолио. Разработка сайтов, SPA, дизайн, верстка, программирование.',
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content:
+          'HTML5, CSS3, PHP, JavaScript, Jquery, Vue, Nuxt, Git, LESS/SASS/SCSS, Flexbox, Grid, Adaptive.',
+      },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+  },
 
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [
-        // https://go.nuxtjs.dev/axios
-        '@nuxtjs/axios',
-        '@nuxtjs/proxy',
-    ],
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['@/assets/scss/main'],
 
-    // Axios module configuration: https://go.nuxtjs.dev/config-axios
-    axios: {
-        // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        proxy: true,
-    },
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
 
-    proxy: ['http://denisobolevich/api'],
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
 
-    router: {
-        linkActiveClass: 'active',
-        linkExactActiveClass: 'exact-active',
-        middleware: ['routerBeforeEach'],
-    },
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
 
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {},
-    env: {
-        test: 'test',
-    },
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    proxy: true,
+  },
+
+  proxy: ['http://denisobolevich/api'],
+
+  router: {
+    linkActiveClass: 'active',
+    linkExactActiveClass: 'exact-active',
+    middleware: ['routerBeforeEach'],
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+  env: {
+    test: 'test',
+  },
+
+  loading: {
+    color: '#fbbf24',
+    height: '2px',
+  },
 }
