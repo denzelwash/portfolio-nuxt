6bc14b639dc2ab7fe8291db06e338e50cf84796d
Denis Obolevich <denzelwash33@mail.ru>
2023-04-17 21:59:21 +0300

    Добавил иконку уведомления

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   // ssr: false,
+  target: 'static',
   head: {
     title: 'Денис Оболевич ★ Портфолио',
     htmlAttrs: {
