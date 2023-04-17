0b05c5dc9a61be86d3bebdbc965ed667a8571fc5
Denis Obolevich <denzelwash33@mail.ru>
2023-04-17 22:27:41 +0300

    Добавил лоадер

---
+++ nuxt/components/TheLoader.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="loader">
+    <span></span>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.loader {
+  position: fixed;
+  display: block;
+  width: 100vw;
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  z-index: 100;
+}
+
+.loader span {
+  transform: rotateZ(45deg);
+  perspective: 1000px;
+  border-radius: 50%;
+  width: 48px;
+  height: 48px;
+  color: #fff;
+}
+
+.loader span::before,
+.loader span::after {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: inherit;
+  height: inherit;
+  border-radius: 50%;
+  transform: rotateX(70deg);
+  animation: 1s spin linear infinite;
+  color: #6e6e6e;
+}
+
+.loader span::after {
+  color: #fcd34d;
+  transform: rotateY(70deg);
+  animation-delay: 0.4s;
+}
+
+@keyframes rotate {
+  0% {
+    transform: translate(-50%, -50%) rotateZ(0deg);
+  }
+  100% {
+    transform: translate(-50%, -50%) rotateZ(360deg);
+  }
+}
+
+@keyframes rotateccw {
+  0% {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  100% {
+    transform: translate(-50%, -50%) rotate(-360deg);
+  }
+}
+
+@keyframes spin {
+  0%,
+  100% {
+    box-shadow: 0.2em 0px 0 0px currentcolor;
+  }
+  12% {
+    box-shadow: 0.2em 0.2em 0 0 currentcolor;
+  }
+  25% {
+    box-shadow: 0 0.2em 0 0px currentcolor;
+  }
+  37% {
+    box-shadow: -0.2em 0.2em 0 0 currentcolor;
+  }
+  50% {
+    box-shadow: -0.2em 0 0 0 currentcolor;
+  }
+  62% {
+    box-shadow: -0.2em -0.2em 0 0 currentcolor;
+  }
+  75% {
+    box-shadow: 0px -0.2em 0 0 currentcolor;
+  }
+  87% {
+    box-shadow: 0.2em -0.2em 0 0 currentcolor;
+  }
+}
+</style>
--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="page-wrapper">
+    <Transition>
+      <TheLoader v-if="loader" />
+    </Transition>
     <TheHeader />
     <main class="page-main">
       <div class="container">
@@ -12,11 +15,33 @@
 
 <script>
 export default {
+  data() {
+    return {
+      loader: true,
+    }
+  },
+  head: {
+    bodyAttrs: {
+      class: 'overflow-hidden',
+    },
+  },
   mounted() {
     const vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
+    document.querySelector('body').classList.remove('overflow-hidden')
+    this.loader = false
   },
 }
 </script>
 
-<style></style>
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
