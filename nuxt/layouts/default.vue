28a40be754abd092c028e06f2c8ca3f1edb353aa
Denis Obolevich <denzelwash33@yandex.ru>
2023-04-29 19:33:33 +0300

    Правка лоадера

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -23,6 +23,9 @@ export default {
   mounted() {
     const vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
+    window.addEventListener('load', () => {
+      this.loader = false
+    })
   },
 }
 </script>
